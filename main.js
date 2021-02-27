9b139e6aa9e20e6ec3a5ec25425e8e58f89e4ff3
Jiin Park <demon0315@naver.com>
2021-02-27 22:21:48 +0900

    App - 함수를 이용해서 정리 정돈하기

--- main.js
+++ main.js
@@ -2,69 +2,64 @@ const http = require("http");
 const fs = require("fs");
 const url = require("url");
 
+function templateHTML(title, list, body) {
+  return `
+    <!doctype html>
+    <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        ${list}
+        ${body}
+      </body>
+    </html>
+  `;
+}
+
+function templateList(fileList) {
+  let list = "<ul>\n";
+  fileList.forEach((fileName) => {
+    list += `\t<li><a href="/?id=${fileName}">${fileName}</a></li>\n`;
+  });
+  list += "</ul>";
+  return list;
+}
+
 const app = http.createServer((request, response) => {
   let _url = request.url;
   const queryData = url.parse(_url, true).query;
   const pathname = url.parse(_url, true).pathname;
 
-  if (pathname == "/") {
-    if (queryData.id === undefined) {
-      fs.readdir("./data", "utf-8", (err, fileList) => {
-        let list = "<ul>";
-        fileList.forEach((fileName) => {
-          list += `<li><a href="/?id=${fileName}">${fileName}</a></li>`;
-        });
-        list += "</ul>";
-
+  if (pathname === "/") {
+    fs.readdir("./data", "utf-8", (err, fileList) => {
+      if (queryData.id === undefined) {
         const title = "Welcome";
         const description = "Hello, Node.js";
-        const template = `
-        <!doctype html>
-        <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-        </html>
-        `;
+        const list = templateList(fileList);
+        const template = templateHTML(
+          title,
+          list,
+          `<h2>${title}</h2><p>${description}</p>`
+        );
         response.writeHead(200);
         response.end(template);
-      });
-    } else {
-      fs.readdir("./data", "utf-8", (err, fileList) => {
-        let list = "<ul>";
-        fileList.forEach((fileName) => {
-          list += `<li><a href="/?id=${fileName}">${fileName}</a></li>`;
-        });
-        list += "</ul>";
+      } else {
         fs.readFile(`data/${queryData.id}`, "utf8", (err, description) => {
           const title = queryData.id;
-          const template = `
-          <!doctype html>
-          <html>
-            <head>
-              <title>WEB1 - ${title}</title>
-              <meta charset="utf-8">
-            </head>
-            <body>
-              <h1><a href="/">WEB</a></h1>
-              ${list}
-              <h2>${title}</h2>
-              <p>${description}</p>
-            </body>
-          </html>
-        `;
+          const list = templateList(fileList);
+          const template = templateHTML(
+            title,
+            list,
+            `<h2>${title}</h2><p>${description}</p>`
+          );
           response.writeHead(200);
           response.end(template);
         });
-      });
-    }
+      }
+    });
   } else {
     response.writeHead(404);
     response.end("not found");
