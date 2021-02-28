f2c4a214bf4576c0fc3a2c3a2e5f7552a53b8359
Jiin Park <demon0315@naver.com>
2021-02-28 14:29:37 +0900

    App 제작 - 모듈의 활용

--- main.js
+++ main.js
@@ -3,33 +3,7 @@ const fs = require("fs");
 const url = require("url");
 const qs = require("querystring");
 
-const template = {
-  HTML: function (title, list, body, control) {
-    return `
-      <!doctype html>
-      <html>
-        <head>
-          <title>WEB1 - ${title}</title>
-          <meta charset="utf-8">
-        </head>
-        <body>
-          <h1><a href="/">WEB</a></h1>
-          ${list}
-          ${control}
-          ${body}
-        </body>
-      </html>
-    `;
-  },
-  list: function (fileList) {
-    let list = "<ul>\n";
-    fileList.forEach((fileName) => {
-      list += `\t<li><a href="/?id=${fileName}">${fileName}</a></li>\n`;
-    });
-    list += "</ul>";
-    return list;
-  },
-};
+const template = require("./lib/template.js");
 
 const app = http.createServer((request, response) => {
   let _url = request.url;
